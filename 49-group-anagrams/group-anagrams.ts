3746537ad82bc07ddda00c1148baf559bbd7cedf
Javi <94010985+hobbie71@users.noreply.github.com>
2025-09-30 13:26:29 -0400

    Time: 30 ms (59.74%) | Memory: 68 MB (65.68%) - LeetSync

--- 49-group-anagrams/group-anagrams.ts
+++ 49-group-anagrams/group-anagrams.ts
@@ -1,41 +1,10 @@
 function groupAnagrams(strs: string[]): string[][] {
-    const output: string[][] = [[strs[0]]]
+    const count: Record<string, string[]> = {}
 
-    for (let i = 1; i < strs.length; i++) {
-        const str = strs[i]
-        let grouped = false
-        
-        for (let j = 0; j < output.length; j++) {
-            if (isAnagram(str, output[j][0])) {
-                output[j].push(str)
-                grouped = true
-                break
-            }
-        }
+    for (let str of strs) {
+        const key = str.split("").sort().join("")
+        count[key] ? count[key].push(str) : count[key] = [str]
+    }    
 
-        if (!grouped) {
-            output.push([str])
-        }
-    }
-
-    return output
+    return Object.values(count)
 };
-
-const isAnagram = (str1: string, str2: string): boolean => {
-    if (str1.length !== str2.length) return false
-
-    const count = {}
-
-    for (let char of str1) {
-        count[char] ? count[char] += 1 : count[char] = 1
-    }
-
-    for (let char of str2) {
-        if (!count[char] || count[char] === 0)
-            return false
-
-        count[char] -= 1
-    }
-
-    return true
-}
