ed142ac022716b398c0ed94430bff9367c91b53b
Javi <94010985+hobbie71@users.noreply.github.com>
2025-10-15 15:11:35 -0400

    Time: 148 ms (12.08%) | Memory: 64.5 MB (5.35%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.ts
@@ -0,0 +1,27 @@
+function characterReplacement(s: string, k: number): number {
+    
+    const map = new Map<string, number>()
+    let output = 0
+    
+    let slow = 0
+    let fast = 0
+
+    while (fast <= s.length) {
+        const windowLength = fast - slow
+        let replacementsUsed = windowLength - Math.max(...map.values())
+
+        if (Math.abs(replacementsUsed) === Infinity) replacementsUsed = 0
+        
+        if (replacementsUsed > k) {
+            map.set(s[slow], (map.get(s[slow]) || 0) - 1)
+            slow++
+        } else {
+            output = Math.max(output, windowLength)
+            map.set(s[fast], (map.get(s[fast]) || 0) + 1)
+            fast++
+        }
+
+    }
+
+    return output
+};
