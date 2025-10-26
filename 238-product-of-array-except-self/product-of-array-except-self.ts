58c5c26afb12f1379351d181e1c92bc8ef181d90
Javi <94010985+hobbie71@users.noreply.github.com>
2025-10-25 21:39:37 -0400

    Time: 7 ms (57.59%) | Memory: 69.8 MB (75.66%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.ts
+++ 238-product-of-array-except-self/product-of-array-except-self.ts
@@ -1,33 +1,16 @@
 function productExceptSelf(nums: number[]): number[] {
-    // Create prefix
-    const prefix = new Map<number, number>()
+    const output: number[] = new Array(nums.length).fill(1)
 
+    let product = 1
     for (let i = 0; i < nums.length; i++) {
-        prefix.set(i, (prefix.get(i - 1) ?? 1) * nums[i])
+        output[i] *= product
+        product *= nums[i]
     }
-
-    // Create postfix
-    const postfix = new Map<number, number>()
-
+    
+    product = 1
     for (let i = nums.length - 1; i >= 0; i--) {
-        postfix.set(i, (postfix.get(i + 1) ?? 1) * nums[i])
-    }
-    // Create output
-
-    const output: number[] = []
-
-    for (let i = 0; i < nums.length; i++) {
-        let product: number
-
-        if (i === 0) {
-            product = postfix.get(i + 1)
-        } else if (i === nums.length - 1) {
-            product = prefix.get(i - 1)
-        } else {
-            product = postfix.get(i + 1) * prefix.get(i - 1)
-        }
-
-        output.push(product)
+        output[i] *= product
+        product *= nums[i]
     }
 
     return output
