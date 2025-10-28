e309acbf614aad46c53da28829d3f71b1e71cdb4
Javi <94010985+hobbie71@users.noreply.github.com>
2025-10-28 16:52:20 -0400

    Time: 8 ms (5.61%) | Memory: 58.4 MB (5.34%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.ts
@@ -0,0 +1,29 @@
+function search(nums: number[], target: number): number {
+    let left = 0
+    let right = nums.length - 1
+
+    while (left <= right) {
+        const middle = Math.floor((right + left) / 2)
+        console.log("left: " + left)
+        console.log("right: " + right)
+        console.log("middle: " + middle)
+
+        if (nums[middle] === target) return middle
+
+        if (nums[left] <= nums[middle]) {
+            if (nums[middle] < target || target < nums[left]) {
+                left = middle + 1
+            } else {
+                right = middle - 1
+            }
+        } else {
+            if (nums[right] < target || target < nums[middle]) {
+                right = middle - 1
+            } else {
+                left = middle + 1
+            } 
+        }
+    }
+
+    return -1
+};
