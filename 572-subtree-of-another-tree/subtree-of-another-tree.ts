8f70e54a7dad59ba351d6c9b13e8968c70888a95
Javi <94010985+hobbie71@users.noreply.github.com>
2025-11-15 15:37:33 -0500

    Time: 7 ms (52.69%) | Memory: 63.5 MB (23.23%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.ts
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+    if (!root || !subRoot) return true
+
+    const queue: TreeNode[] = [root]
+
+    while (queue.length) {
+        const node = queue.shift()
+
+        if (node.val === subRoot.val) {
+            if (isSameTree(node, subRoot)) return true
+        }
+
+        if (node.left) queue.push(node.left)
+        if (node.right) queue.push(node.right)
+    }
+
+    return false
+
+    function isSameTree(node1: TreeNode | null, node2: TreeNode | null): boolean {
+        if (!node1 && !node2) return true
+        if (!node1 || !node2) return false
+        return node1.val === node2.val && isSameTree(node1.left, node2.left) && isSameTree(node1.right, node2.right)
+    }
+};
