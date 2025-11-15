b04f2585e88637ce50bd941e4368977273184057
Javi <94010985+hobbie71@users.noreply.github.com>
2025-11-15 15:44:26 -0500

    Time: 8 ms (34.84%) | Memory: 63.6 MB (18.13%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.ts
+++ 572-subtree-of-another-tree/subtree-of-another-tree.ts
@@ -31,6 +31,7 @@ function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
     function isSameTree(node1: TreeNode | null, node2: TreeNode | null): boolean {
         if (!node1 && !node2) return true
         if (!node1 || !node2) return false
-        return node1.val === node2.val && isSameTree(node1.left, node2.left) && isSameTree(node1.right, node2.right)
+        if (node1.val !== node2.val) return false
+        return isSameTree(node1.left, node2.left) && isSameTree(node1.right, node2.right)
     }
 };
