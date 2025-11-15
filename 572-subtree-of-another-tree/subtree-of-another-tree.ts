9651e9a1b6d2a7d0c82393274802f706881e578e
Javi <94010985+hobbie71@users.noreply.github.com>
2025-11-15 15:48:47 -0500

    Time: 5 ms (86.69%) | Memory: 62.9 MB (59.77%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.ts
+++ 572-subtree-of-another-tree/subtree-of-another-tree.ts
@@ -13,25 +13,17 @@
  */
 
 function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
-    if (!root || !subRoot) return true
+    if (!root && !subRoot) return true
+    if (!root || !subRoot) return false
 
-    const queue: TreeNode[] = [root]
+    if (isSameTree(root, subRoot)) return true
 
-    while (queue.length) {
-        const node = queue.shift()
-
-        if (isSameTree(node, subRoot)) return true
-
-        if (node.left) queue.push(node.left)
-        if (node.right) queue.push(node.right)
-    }
-
-    return false
-
-    function isSameTree(node1: TreeNode | null, node2: TreeNode | null): boolean {
-        if (!node1 && !node2) return true
-        if (!node1 || !node2) return false
-        if (node1.val !== node2.val) return false
-        return isSameTree(node1.left, node2.left) && isSameTree(node1.right, node2.right)
-    }
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
 };
+
+function isSameTree(node1: TreeNode | null, node2: TreeNode | null): boolean {
+    if (!node1 && !node2) return true
+    if (!node1 || !node2) return false
+    if (node1.val !== node2.val) return false
+    return isSameTree(node1.left, node2.left) && isSameTree(node1.right, node2.right)
+}
