63ee06162bba4ca2502e39dd154a38d794e0b2dd
Javi <94010985+hobbie71@users.noreply.github.com>
2025-11-19 15:21:30 -0500

    Time: 1 ms (40.91%) | Memory: 75.7 MB (11.74%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function countNodes(root: TreeNode | null): number {
+    let output = 0
+
+    function dfs(node: TreeNode | null) {
+        if (!node) return
+
+        output++
+
+        dfs(node.left)
+        dfs(node.right)
+    }
+
+    dfs(root)
+    return output
+};
