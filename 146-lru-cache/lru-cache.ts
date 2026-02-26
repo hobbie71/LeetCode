cc6414ec59ed16f48521053732c00b533f8b6d90
Javi <94010985+hobbie71@users.noreply.github.com>
2026-02-26 08:40:55 -0500

    Time: 123 ms (16.93%) | Memory: 108.5 MB (63.62%) - LeetSync

--- 146-lru-cache/lru-cache.ts
+++ 146-lru-cache/lru-cache.ts
@@ -1,14 +1,15 @@
 class LRUCache {
-    cache: Map<number, number>
-    capacity: number
+    private capacity: number
+    private cache: Map<number, number>
 
     constructor(capacity: number) {
-        this.cache = new Map()
         this.capacity = capacity
+        this.cache = new Map()
     }
 
     get(key: number): number {
         if (!this.cache.has(key)) return -1
+
         const value = this.cache.get(key)
         this.cache.delete(key)
         this.cache.set(key, value)
@@ -18,11 +19,13 @@ class LRUCache {
     put(key: number, value: number): void {
         if (this.cache.has(key)) {
             this.cache.delete(key)
-        } else if (this.cache.size === this.capacity) {
-            const deleteKey = this.cache.keys().next().value
-            this.cache.delete(deleteKey)
-        } 
-        
+        } else {
+            if (this.cache.size === this.capacity) {
+                const firstKey = this.cache.keys().next().value
+                this.cache.delete(firstKey)
+            }
+        }
+
         this.cache.set(key, value)
     }
 }
