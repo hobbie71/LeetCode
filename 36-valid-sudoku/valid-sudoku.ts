18dc6df06931c5cf14463774fa5a5326534a165f
Javi <94010985+hobbie71@users.noreply.github.com>
2026-03-13 21:23:23 -0400

    Time: 4 ms (77.96%) | Memory: 58.8 MB (73.09%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.ts
+++ 36-valid-sudoku/valid-sudoku.ts
@@ -1,47 +1,37 @@
 function isValidSudoku(board: string[][]): boolean {
+    const rowSet: Set<string> = new Set()
     const colMap: Map<number, string> = new Map()
-    const rowMap: Map<number, string> = new Map()
-    const gridMap: Map<string, string> = new Map()
+    const squareMap: Map<string, string> = new Map()
 
     for (let row = 0; row < board.length; row++) {
-        for (let col = 0; col < board.length; col++) {
-            const value: string = board[row][col]
+        const rows = board[row]
 
+        for (let col = 0; col < rows.length; col++) {
+            const value = rows[col]
             if (value === ".") continue
 
-            // Check & Store col
-            const currentCol: string = colMap.get(col)
-            if (!currentCol) {
-                colMap.set(col, value)
-            } else {
-                if (currentCol.includes(value)) return false
-                colMap.set(col, currentCol + value)
-            }
-            
-            // Check & Store row
-            const currentRow: string = rowMap.get(row)
-            if (!currentRow) {
-                rowMap.set(row, value)
-            } else {
-                if (currentRow.includes(value)) return false
-                rowMap.set(row, currentRow + value)
-            }
+            // Check Row
+            if (rowSet.has(value)) return false
+            rowSet.add(value)
 
-            // Check & Store 3x3
-            const key = createGridKey(col, row)
-            const currentGrid: string = gridMap.get(key)
-            if (!currentGrid) {
-                gridMap.set(key, value)
-            } else {
-                if (currentGrid.includes(value)) return false
-                gridMap.set(key, currentGrid + value)
-            }
-        }   
+            // Check col
+            const colValue = colMap.get(col)
+            if (colValue && colValue.includes(value)) return false
+            colMap.set(col, colValue + value)
+
+            // Check square
+            const key = getSquareKey(col, row)
+            const squareValue = squareMap.get(key)
+            if (squareValue && squareValue.includes(value)) return false
+            squareMap.set(key, squareValue + value)
+        }
+
+        rowSet.clear()
     }
 
     return true
 };
 
-const createGridKey = (col: number, row: number): string => {
+const getSquareKey = (col: number, row: number): string => {
     return `${Math.floor(col / 3)}-${Math.floor(row / 3)}`
 }
