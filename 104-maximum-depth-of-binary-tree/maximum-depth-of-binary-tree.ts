33a32018fed9fcf04ba541bcf24044405deb3403
Javi <94010985+hobbie71@users.noreply.github.com>
2026-03-17 17:37:35 -0400

    Time: 0 ms (100.00%) | Memory: 58.5 MB (83.25%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.ts
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.ts
@@ -13,6 +13,6 @@
  */
 
 function maxDepth(root: TreeNode | null): number {
-    if (!root) return 0
-    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+    if (!root) return 0;
+    return Math.max(1 + maxDepth(root.left), 1 + maxDepth(root.right));
 };
